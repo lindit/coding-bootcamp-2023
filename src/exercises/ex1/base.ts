55605faa2b3561e4169e45cae0c18d937d0d0b8e
Lindit Ajvazaj <lindit.ajvazaj@valtech.com>
2023-03-11 17:52:08 +0100

    setup ex1

---
+++ src/exercises/ex1/base.ts
@@ -0,0 +1,31 @@
+/**
+ * Create an interface IPerson with the following properties:
+ * firstName (string)
+ * lastName (string)
+ * age (number)
+ */
+
+// TODO: YOUR CODE HERE
+
+/**
+ * Create an array of type IPerson[] called people.
+ */
+
+// TODO: YOUR CODE HERE
+
+/**
+ * Create three instances of IPerson
+ */
+// TODO: YOUR CODE HERE
+
+/**
+ * Push the three instances of IPerson into the people array.
+ */
+// TODO: YOUR CODE HERE
+
+/**
+ * Log all three entries of people using string interpolation in the format
+ * firstName: XXX, lastname: XXX, age: xxx
+ * use only the people variable like people[0]
+ */
+// TODO: YOUR CODE HERE
